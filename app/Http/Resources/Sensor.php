7298e05b463110f0b0794fbb99a5e83e10f8a905
guillermoLeon26 <gleon2611@gmail.com>
2018-11-02 21:32:54 -0500

    Dos sensores Scada

--- app/Http/Resources/Sensor.php
+++ app/Http/Resources/Sensor.php
@@ -14,11 +14,12 @@ class Sensor extends JsonResource
    */
   public function toArray($request){
     return [
-      'id'      =>  $this->id,
-      'tipo'    =>  $this->tipo->valor,
-      'unidad'  =>  $this->unidad->valor,
-      'datos'   =>  [25, 26, 27, 28, 30, 29, 40, 35],
-      'fecha'   =>  ['22:08:10', '22:08:15', '22:08:20', '22:08:25', '22:08:30', '22:08:35', '22:08:40', '22:08:45']
+      'id'        =>  $this->id,
+      'tipo'      =>  $this->tipo->valor,
+      'unidad'    =>  $this->unidad->valor,
+      'ubicacion' =>  $this->ubicacion,
+      'datos'     =>  [],
+      'fecha'     =>  []
     ];
   }
 }
